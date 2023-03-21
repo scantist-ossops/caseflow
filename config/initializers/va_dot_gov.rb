60bd8f47c59db810a9e97afe641d16d81c25a88a
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2023-03-20 21:41:05 -0400

    Cache initialized service to span reloads

--- config/initializers/va_dot_gov.rb
+++ config/initializers/va_dot_gov.rb
@@ -8,4 +8,4 @@ def use_fake_va_dot_gov_service?
   env_vars_present? ? true : ApplicationController.dependencies_faked?
 end
 
-VADotGovService = use_fake_va_dot_gov_service? ? Fakes::VADotGovService : ExternalApi::VADotGovService
+VADotGovService ||= use_fake_va_dot_gov_service? ? Fakes::VADotGovService : ExternalApi::VADotGovService
