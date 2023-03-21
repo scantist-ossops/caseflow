434ca205c2cf8a093e9b1f9a8efd16bb20f8ede3
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2023-03-20 21:32:07 -0400

    Allow for VA.gov sandbox use in local envs

--- config/initializers/va_dot_gov.rb
+++ config/initializers/va_dot_gov.rb
@@ -1,1 +1,11 @@
-VADotGovService = (ApplicationController.dependencies_faked? ? Fakes::VADotGovService : ExternalApi::VADotGovService)
+# frozen_string_literal: true
+
+def env_vars_present?
+  ENV["VA_DOT_GOV_API_URL"] && ENV["VA_DOT_GOV_API_KEY"]
+end
+
+def use_fake_va_dot_gov_service?
+  env_vars_present? ? true : ApplicationController.dependencies_faked?
+end
+
+VADotGovService = use_fake_va_dot_gov_service? ? Fakes::VADotGovService : ExternalApi::VADotGovService
