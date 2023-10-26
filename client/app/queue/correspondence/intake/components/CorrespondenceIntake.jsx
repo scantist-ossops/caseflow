a5702e7bd7424ecb73b59565256c736b8304ed3a
divyadasari-va <divya.dasari@va.gov>
2023-10-26 09:23:01 -0600

    Initial changes to 29403

--- client/app/queue/correspondence/intake/components/CorrespondenceIntake.jsx
+++ client/app/queue/correspondence/intake/components/CorrespondenceIntake.jsx
@@ -1,7 +1,7 @@
-import React, { useMemo } from 'react';
-import PropTypes from 'prop-types';
+import React, { useMemo, useState } from 'react';
 import ProgressBar from 'app/components/ProgressBar';
 import Button from '../../../../components/Button';
+import RadioField from '../../../../components/RadioField';
 
 const progressBarSections = [
   {
@@ -18,6 +18,13 @@ const progressBarSections = [
   },
 ];
 
+const priorMailanswer = [
+  { displayText: 'Yes',
+    value: 'yes' },
+  { displayText: 'No',
+    value: 'no' }
+];
+
 export const CorrespondenceIntake = () => {
   const sections = useMemo(
     () =>
@@ -26,9 +33,35 @@ export const CorrespondenceIntake = () => {
         current
       })),
   );
+  const [selectedValue, setSelectedValue] = useState('no');
+
+  const handleRadioChange = (event) => {
+    setSelectedValue(event);
+  };
 
   return <div>
     <ProgressBar sections={sections} />
+    <section className="cf-app-segment cf-app-segment--alt">
+      <h1>Add Related Correspondence</h1>
+      <p>Add any related correspondence to the mail package that is in progress.</p>
+
+      <h3>Associate with prior Mail</h3>
+
+      <p>Is this correspondence related to prior mail?</p>
+      <RadioField
+        name=""
+        options={priorMailanswer}
+        value={selectedValue}
+        onChange={handleRadioChange} />
+
+      {selectedValue === 'yes' && (
+        <div className="cf-app-segment cf-app-segment--alt">
+          <p>Please select the prior mail to link to this correspondence</p>
+          <p>Viewing 1-15 out of 200 total</p>
+        </div>
+      )}
+
+    </section>
     <Button
       name="Cancel"
       classNames={['cf-btn-link']} />
@@ -41,4 +74,3 @@ export const CorrespondenceIntake = () => {
   </div>;
 };
 export default CorrespondenceIntake;
-
